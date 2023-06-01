b890b489a312c6ebed9ece3211f6aeb0cf228939
maksonchik20 <elena-g0903@mail.ru>
2023-06-01 23:12:42 +0300

    Куча add sift_down

--- Куча.py
+++ Куча.py
@@ -7,8 +7,23 @@ class Queue:
             self.queue[element // 2], self.queue[element] = self.queue[element], self.queue[element // 2]
             element = element // 2
 
-    def sift_down(self, ind):
-        if ind * 2
+    def sift_down(self, v):
+        while True:
+            if v * 2 <= len(self.queue):
+                if v * 2 + 1 == len(self.queue):
+                    max_idx = v * 2
+                elif self.queue[v * 2] <= self.queue[v * 2 + 1]:
+                    max_idx = v * 2
+                else:
+                    max_idx = v * 2 + 1
+                
+                if self.queue[v] > self.queue[max_idx]:
+                    self.queue[v], self.queue[max_idx] = self.queue[max_idx], self.queue[v]
+                    self.sift_down(max_idx)
+                else:
+                    break
+            else:
+                break
 
     def get_min(self):
         return self.queue[1]
@@ -19,13 +34,16 @@ class Queue:
     
     def delete_min(self):
         self.queue[1], self.queue[-1] = self.queue[-1], self.queue[1]
+        self.queue.pop()
         self.sift_down(1)
 
 queue = Queue()
 queue.add_element(5)
 queue.add_element(8)
-print(queue.queue)
-print(queue.get_min())
 queue.add_element(2)
 queue.add_element(6)
+queue.delete_min()
+print(queue.queue)
+queue.delete_min()
+print(queue.queue)
 print(queue.get_min())
